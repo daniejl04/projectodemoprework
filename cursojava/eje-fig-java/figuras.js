bc29dfd3fb8d969ab46b79707e6b6d27a8b19ad7
daniejl <danielestebanjimenezlopez@gmail.com>
2022-03-09 21:59:11 -0500

    agg formulas cuadro triangulo

--- cursojava/eje-fig-java/figuras.js
+++ cursojava/eje-fig-java/figuras.js
@@ -1,1 +1,8 @@
-console.log ("hola mundo")
+const ladocuadrado = 5;
+console.log ("los lados del cuadrado miden: " + ladocuadrado);
+
+const perimetrocuadrado = ladocuadrado * 4;
+console.log ("el perimetro del cuadrado es: " + ladocuadrado);
+
+const areacuadrado = ladocuadrado * ladocuadrado;
+console.log ("el perimetro del cuadrado es: " + ladocuadrado);
