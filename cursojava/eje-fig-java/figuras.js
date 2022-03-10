682346bf809f9bc6c605f215e60018f9341f4dfb
daniejl <danielestebanjimenezlopez@gmail.com>
2022-03-09 22:02:22 -0500

    agg formulas cuadro triangulo 2

--- cursojava/eje-fig-java/figuras.js
+++ cursojava/eje-fig-java/figuras.js
@@ -1,8 +1,30 @@
+//codigo del cuadraro
 const ladocuadrado = 5;
-console.log ("los lados del cuadrado miden: " + ladocuadrado);
+console.log ("los lados del cuadrado miden: " + ladocuadrado + "cm");
 
 const perimetrocuadrado = ladocuadrado * 4;
-console.log ("el perimetro del cuadrado es: " + ladocuadrado);
+console.log ("el perimetro del cuadrado es: " + ladocuadrado + "cm");
 
 const areacuadrado = ladocuadrado * ladocuadrado;
-console.log ("el perimetro del cuadrado es: " + ladocuadrado);
+console.log ("el area del cuadrado es: " + ladocuadrado + "cm^2");
+
+// codigo del triangulo
+
+const ladotriangulo1 = 5;
+const ladotriangulo2 = 5;
+const basetriangulo = 4;
+console.log ("los lados del triangulo miden: " 
++ ladotriangulo1 
++ "cm," 
++ ladotriangulo2 
++ "cm," 
++ basetriangulo 
++ "cm" 
+);
+
+const perimetrocuadrado = ladocuadrado * 4;
+console.log ("el perimetro del cuadrado es: " + ladocuadrado + "cm");
+
+const areacuadrado = ladocuadrado * ladocuadrado;
+console.log ("el area del cuadrado es: " + ladocuadrado + "cm^2");
+
