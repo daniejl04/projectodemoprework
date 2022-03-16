2d31db091c657654bd817aa521b009ed51742d61
daniejl <danielestebanjimenezlopez@gmail.com>
2022-03-15 20:37:44 -0500

    triangulo isoceles

--- cursojava/eje-fig-java/figuras.js
+++ cursojava/eje-fig-java/figuras.js
@@ -80,7 +80,22 @@ function areacirculo(radio){
 }
 console.groupEnd();
 
+function alturaTrianguloIsosceles(trianguloGrandeLadoA, trianguloGrandeLadoB, trianguloGrandeLadoBase) {
+    if (trianguloGrandeLadoA != trianguloGrandeLadoB) {
+        console.error("Los lados a y b no son iguales");
+    } else {
+        const trianguloPequenoLadoB = trianguloGrandeLadoBase / 2;
+        const trianguloPequenoLadoBase = trianguloGrandeLadoA;
 
+        const trianguloPequenoLadoBCuadrado = trianguloPequenoLadoB * trianguloPequenoLadoB;
+        const trianguloPequenoLadoBaseCuadrado = trianguloPequenoLadoBase * trianguloPequenoLadoBase;
+
+        const trianguloPequenoLadoA = Math.sqrt(trianguloPequenoLadoBaseCuadrado - trianguloPequenoLadoBCuadrado);
+
+        const trianguloGrandeAltura = trianguloPequenoLadoA;
+        return trianguloGrandeAltura;
+    }
+}
 //interacion html
 function calcularPerimetroCuadrado(){
      const input = document.getElementById("inputcuadrado");
