69c83c3d284c72105c52a14c5ae02614143dc41e
daniejl <danielestebanjimenezlopez@gmail.com>
2022-03-16 22:01:13 -0500

    termina fase de integracion java html

--- cursojava/porce-descuentos/descuentos.js
+++ cursojava/porce-descuentos/descuentos.js
@@ -9,6 +9,22 @@ function calcularpreciocondescuento(precio, descuento) {
     
     return precioConDescuento;
 }
+
+
+function onclickButtonPriceDiscount(){
+    const inputPrice =  document.getElementById ("inputPrice")
+    const pricevalue = inputPrice.value;
+
+    const inputDiscount =  document.getElementById ("inputDiscount")
+    const DiscountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularpreciocondescuento (pricevalue, DiscountValue)
+
+    const resultp = document.getElementById ("resultp");
+    resultp.innerText = "este es tu precio con descuento" + precioConDescuento;
+}
+
+
 // const porcentajePrecioDescuento = 100 - descuento;
 
 // const precioConDescuento = (precioOriginal * porcentajePrecioDescuento) / 100;
